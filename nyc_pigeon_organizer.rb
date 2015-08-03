d38686ebc74149ab9fe2b61e7e39fb74f08f1f5f
jessicashannon <jessdahling@gmail.com>
2015-08-03 15:08:26 -0400

    done

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,42 @@
+require 'pry'
+
+pigeon_data = {
+  :color => {
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+}
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  list = {}
+  data.each do |attribute, sub_attributes|
+    sub_attributes.each do |sub_attribute, names|
+      names.each do |name|
+        if list.has_key?(name)
+          if list[name].has_key?(attribute)
+            list[name][attribute] << sub_attribute.to_s
+          else
+            list[name][attribute] = [sub_attribute.to_s]
+          end
+        else
+        list[name] = {attribute => [sub_attribute.to_s]}
+        end
+      end
+    end
+  end
+  list
 end
+
+
+puts nyc_pigeon_organizer(pigeon_data)
